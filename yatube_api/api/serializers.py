0a87304a23923b34f3296b5e209383c3f51ad176
Вероника Авдеева <veron.avdeeva2015@yandex.ru>
2023-04-26 00:55:20 +0300

    1

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
+from posts.models import Comment, Follow, Group, Post, User
 
-from posts.models import Comment, Post
+from .serializers import SlugRelatedField
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -21,3 +22,37 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault())
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all())
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = (
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message=('Вы уже подписаны на автораю')
+            ),
+        )
+
+    def validate(self, data):
+        if data['user'] == data['following']:
+            raise serializers.ValidationError(
+                'Самоподписка невозможна.')
+        return data
